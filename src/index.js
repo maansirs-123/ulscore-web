2a5bf2fb3caf2f73d0653d4c37443f7ab784a598
maansirs-123 <maansirs@tamu.edu>
2026-06-23 12:27:24 -0500

    Update React API configuration

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles.css";
 
-<link rel="apple-touch-icon" href="%PUBLIC_URL%/apple-touch-icon.png" />
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
